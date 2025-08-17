57352a7a0b96d18808539d3517aeccffceeca5c7
Shahzar Ali <shahzzarralii@gmail.com>
2025-08-17 09:14:31 +0500

    name correction

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/se-hoppingreact",
+  base: "/e-shoppingreact",
   plugins: [react()],
 })
